718834bde4a537f6bafe73bed9601dad7c25ce25
xiaohanchi <xiaohanchi123@gmail.com>
2026-08-11 23:19:22 -0500

    update cont code

--- temp/main.R
+++ temp/main.R
@@ -10,28 +10,28 @@ source("../jags_function.R")
 source("../r_functions.R")
 
 
-
+ 
 ### RUN code ==========================
 source("../scenarios.R")
 all_config <- rbind(
-  # binary y
+  # continuous y
   # 4 sites w/ 5 periods
   expand.grid(
-    y_type = 2,
-    n_sc = c(7:8),
-    beta_trt = c(0, 0.8), 
-    site_delta_sc = c(7:9),
-    drift_sc = c(7:9),
+    y_type = 1,
+    n_sc = c(1:2),
+    beta_trt = c(0, 0.6), 
+    site_delta_sc = c(1:3),
+    drift_sc = c(1:3),
     active_time_sc = c(1),
     adjusted = c(TRUE, FALSE)
   ),
   # 6 sites w/ 8 periods
   expand.grid(
-    y_type = 2,
-    n_sc = c(10:11), 
-    beta_trt = c(0, 0.8), 
-    site_delta_sc = c(10:12),
-    drift_sc = c(10:12),
+    y_type = 1,
+    n_sc = c(4:5), 
+    beta_trt = c(0, 0.5), 
+    site_delta_sc = c(4:6),
+    drift_sc = c(4:6),
     active_time_sc = c(2),
     adjusted = c(TRUE, FALSE)
   )
