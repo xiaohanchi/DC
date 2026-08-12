7f291899a374636f1b74ca794df99b84242b600b
xiaohanchi <xiaohanchi123@gmail.com>
2026-08-12 15:36:22 -0500

    add bin code

--- temp/submit_jobs.sh
+++ temp/submit_jobs.sh
@@ -15,9 +15,10 @@ for i in $(seq 1 ${n_jobs}); do
 
 	cp runjobs.lsf runjobs_tmp.lsf
 	perl -pi -e "s/folder00/jobtype$i/g; s/idx00/$i/g" runjobs_tmp.lsf
-	if test $i -gt $((n_jobs / 3)); then
-		perl -pi -e "s/short/medium/g; s/3:00/4:00/g" runjobs_tmp.lsf
-	fi
+	# if test $i -gt $((n_jobs / 3)); then
+	# 	perl -pi -e "s/short/medium/g; s/3:00/4:00/g" runjobs_tmp.lsf
+	# fi
+	perl -pi -e "s/short/medium/g; s/3:00/24:00/g" runjobs_tmp.lsf
 	mv runjobs_tmp.lsf ./jobtype$i/runjobs.lsf
 
 done
